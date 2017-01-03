5bde9a588d809a5c4afa6e1d9f343655e6a1dbfc
Wenyiqn <Wenyiqn@WenyiqndeiMac.local>
2017-01-03 09:27:21 +0800

    添加web-mode 和 js2-mode 的配置hook

--- austin/at-setting.el
+++ austin/at-setting.el
@@ -27,14 +27,40 @@
 (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
 ;; 添加模式在编辑lisp 的时候成对高亮括号
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters "python3")
+;; (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 
+;; html-mode 开启emmet-mode
+(add-hook 'web-mode-hook 'emmet-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; 配置company 弹出补全延迟时间
 (setq company-idle-delay 0.1)
+
+;; 配置company最小的补全字符
 (setq company-minimum-prefix-length 2)
+
+;; 配置光标的类型
 (setq cursor-type (quote bar))
+
+;; 配置buffer文件系统编码
 (setq default-buffer-file-coding-system (quote utf-8))
+
+;; 关闭默认的开始GNU 欢迎屏幕
 (setq inhibit-startup-screen t)
+
 (setq make-backup-files nil)
 (setq tab-width 4)
+
+;; 配置自动保存的时间戳
 (setq auto-save-interval 100)
+
+;; 配置Python3环境
+(setq python-shell-interpreter "python3")
+(setq python-shell-completion-native-disabled-interpreters '("python3"))
+;; org-mode 执行源代码不需要提示确认
+(setq org-confirm-babel-evaluate nil)
+()
 
 (provide 'at-setting)
