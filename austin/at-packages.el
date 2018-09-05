dc9fe8f1e4fb73cf55f1cbe0bb51a98526a304db
Matcai <matthewdotcai@gmail.com>
2018-09-05 23:30:57 +0800

    1.使用el-get进行包管理; 2.添加helm包进行搜索管理; 3.添加all-the-icons和spaceline-all-the-icons进行neotree文件图标和状态栏美化; 4.添加powerline包进行状态栏美化

--- austin/at-packages.el
+++ austin/at-packages.el
@@ -8,9 +8,9 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives '(
-						   ("gnu" . "http://elpa.zilongshanren.com/gnu/")
-						   ("melpa" . "http://elpa.zilongshanren.com/melpa/")
-						   ))
+			   ("gnu" . "http://elpa.emacs-china.org/gnu/")
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/")
+			   ))
   ;; 采用国内的镜像源
   (package-initialize)
   ;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -18,6 +18,7 @@
   )
 
 (require 'cl)
+
 (defvar jon/packages '(
 					   company
 					   monokai-theme
@@ -27,7 +28,12 @@
 					   switch-window
 					   yasnippet
 					   evil
-					   smex
+					   ;; smex
+                                           helm
+                                           helm-directory
+                                           helm-projectile
+                                           all-the-icons
+                                           spaceline-all-the-icons
 					   smartparens
 					   web-mode
 					   emmet-mode
@@ -35,32 +41,64 @@
 					   exec-path-from-shell
 					   ;; ox-latex-chinese
 					   org2ctex
+                                           powerline
 					   neotree
 					   company-jedi
 					   company-go
 					   go
+                                           json-mode
 					   virtualenv
 					   flycheck
 					   py-autopep8
 					   company-c-headers
+                                           htmlize
+                                           vue-mode
+                                           seti-theme
+                                           material-theme
+                                           ;; tern ;; javascrit 代码分析方法提示补全
 					   ) "Default packages installed")
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; (unless (require 'el-get nil 'noerror)
+;;   (package-refresh-contents)
+;;   (package-install 'el-get)
+;;   (require 'el-get)
+;;   )
+
+;; (setq el-get-recipe-path
+;;       (list (concat (file-name-directory el-get-script) "recipes")
+;;             el-get-recipe-path-elpa))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;; (el-get 'sync)
+;; (el-get-elpa-build-local-recipes (expand-file-name "el-get/recipes/elpa" el-get-dir) t)
+(el-get 'sync)
+(el-get 'sync jon/packages)
+
 ;; 定义需要安装的软件包
-(setq package-selected-packages jon/packages)
+;; (setq package-selected-packages jon/packages)
 
-;; 定义函数用于判断软件包是否安装
-(defun jon/packages-installed-p ()
-  (dolist (pkg jon/packages t)
-    (if (not (package-installed-p pkg)) (return nil))))
+;; ;; 定义函数用于判断软件包是否安装
+;; (defun jon/packages-installed-p ()
+;;   (dolist (pkg jon/packages t)
+;;     (if (not (package-installed-p pkg)) (return nil))))
 
 
-;; 如果软件包没有安装则自动安装。
-(unless (jon/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg jon/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
+;; ;; 如果软件包没有安装则自动安装。
+;; (unless (jon/packages-installed-p)
+;;   (message "%s" "Refreshing package database...")
+;;   (package-refresh-contents)
+;;   (dolist (pkg jon/packages)
+;;     (when (not (package-installed-p pkg))
+;;       (package-install pkg))))
 
 
 (provide 'at-packages)
