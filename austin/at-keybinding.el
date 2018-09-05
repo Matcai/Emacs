4c51946a34dec0e6dadbb66542c5f5c55b8b2515
Matcai <matthewdotcai@gmail.com>
2018-09-05 23:33:33 +0800

    1.helm包按键绑定；2.macOS command按键映射为super按键模式

--- austin/at-keybinding.el
+++ austin/at-keybinding.el
@@ -12,16 +12,28 @@
 ;;     	     |___/     	       	       	       |___/
 
 
-
-(global-set-key (kbd "M-x") 'smex)
+(setq mac-command-modifier 'super)
 
 ;; 绑定一个快捷键F2 快速调用初始化的配置文件
 (global-set-key (kbd "<f2>") 'neotree-toggle)
+
+;; 绑定mac全屏快捷键，Mac系统中emacs默认没有设置，只能通过鼠标点击全屏按钮
+(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
+
 
 (define-key global-map (kbd "C-x <tab>") 'indent-region-or-buffer)
 
 ;; 定义按键切换窗口
 (define-key global-map "\C-x\ o" 'switch-window)			
 
+
+;; ------------------------- helm 设置 -------------------------------
+;; helm-find-files 绑定tab为自动补全文件名， 默认是Ctrl-j为自动补全文件名
+(define-key helm-find-files-map "\t" 'helm-execute-persistent-action)
+;; (define-key helm-M-x-map "\t" 'helm-execute-selection-action)
+
+(define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (provide 'at-keybinding)
